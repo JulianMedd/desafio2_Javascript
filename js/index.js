be37f4058514147a5d666aa88b29ba3fbd39cfd4
Julian Med <julian.medina7@hotmail.com.ar>
2021-01-01 16:06:22 -0300

    desafio numero 2 del curso de javascript

---
+++ js/index.js
@@ -0,0 +1,57 @@
+
+/*bucle tabla de multiplicar*/
+
+var num = prompt("ingrese un numero");
+
+for(var i=0; i<=10; i++){
+    console.log(num * i);
+}
+
+/*funcion para determinar numero primo*/
+
+function saberNumPrimo(num){
+    if(num == 0 || num == 1 || num == 4) return false
+    for(var i=2; i < (num / 2); i++){
+        if( num % i == 0) return false;
+    }
+    return true;
+}
+
+/*funcion para mostrar los primos entre*/
+
+function numerosPrimosEntre(desde,hasta){
+    for(desde; desde <= hasta; desde++){
+        if(saberNumPrimo(desde))
+            console.log(desde);
+    }
+}
+
+/* los 100 numeros primos desde un numero dado */
+
+function numPrimosDesde(numInicial){
+    numLimite = numInicial + 100;
+    for(numInicial; numInicial <= numLimite; numInicial++){
+        if(saberNumPrimo(numInicial)){
+            console.log(numInicial);
+        }
+    }
+}
+
+/*funcion promedio*/
+
+function suma(num1,num2){
+    return num1 + num2;
+}
+
+function promedioEntreDosNumeros(total){
+    return total / 2;
+}
+
+function mostrarPromedio(numero1,numero2){
+    total = suma(numero1, numero2);
+    promedio = promedioEntreDosNumeros(total);
+    alert(promedio);
+}
+
+mostrarPromedio(8, 10);
+numPrimosDesde(5);
